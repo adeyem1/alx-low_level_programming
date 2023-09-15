f9d2482042c06aba8e8877917dc2efc097a7afbe
adeyem1 <adeyem1>
2023-09-14 18:03:20 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,1 +1,16 @@
-we getting there
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigitr(int c);
+int mulr(int a, int b);
+void print_numbersr(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif
